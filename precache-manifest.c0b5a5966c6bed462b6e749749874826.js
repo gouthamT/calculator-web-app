bf4753b8961866b4ccfba9fbea796c27a8aff3f0
goutham.tupakula <goutham.tupakula@4mation.com.au>
2020-07-18 05:32:51 +1000

    Updates

---
+++ precache-manifest.c0b5a5966c6bed462b6e749749874826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e297e4ac6467583e5c50e8970e05dd",
+    "url": "/calculator-web-app/index.html"
+  },
+  {
+    "revision": "3c874f8cbb614aff4343",
+    "url": "/calculator-web-app/static/css/main.185228f3.chunk.css"
+  },
+  {
+    "revision": "e564d48f3e864a7bb337",
+    "url": "/calculator-web-app/static/js/2.7431b1cf.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/calculator-web-app/static/js/2.7431b1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c874f8cbb614aff4343",
+    "url": "/calculator-web-app/static/js/main.8bb0385f.chunk.js"
+  },
+  {
+    "revision": "c401700ffd7257483f0b",
+    "url": "/calculator-web-app/static/js/runtime-main.77fa75c6.js"
+  }
+]);
